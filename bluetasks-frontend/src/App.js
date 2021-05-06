5bd4021bb76dc9b12156ce11f91858d3f8324774
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-06 14:22:21 -0300

    Adicionando bootstrap e criando a classe do componente App

--- bluetasks-frontend/src/App.js
+++ bluetasks-frontend/src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>BlueTasks</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>BlueTasks</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
