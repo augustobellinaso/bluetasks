1fcbfff1ae4fc2e94e0e1ccc6752a6ffc91822da
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-06 16:48:07 -0300

    Adicionando funcionalidade e estilo à barra de navegação

--- bluetasks-frontend/src/App.js
+++ bluetasks-frontend/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 
 class App extends Component {
   //constructor(props) {
-   // super(props);
+  // super(props);
   //}
 
   render() {
     return (
-      <div className="App">
-        <NavBar />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- bluetasks-frontend/src/components/NavBar.js
+++ bluetasks-frontend/src/components/NavBar.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
+import { APP_NAME } from '../constants';
 import NavBarItem from './NavBarItem';
 
 class NavBar extends Component {
 
     constructor(props) {
         super(props);
-        
+
         this.state = {
             items: [
-                {name: "Item 1", href: "/"},
-                {name: "Item 2", href: "/"},
-                {name: "Item 3", href: "/"},
-                {name: "Item 4", href: "/"}
+                { name: "Listar Tarefas", href: "/" },
+                { name: "Nova Tarefa", href: "/form" }
             ]
         }
     }
-    
+
+    onClickHandler(item) {
+        alert(item.name);
+    }
+
 
     render() {
         return (
             <div>
-                {this.state.items.map((i) => <NavBarItem name={i.name} />)}        
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <span className="navbar-brand mb-0 h1">{APP_NAME}</span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <div className="navbar-nav mr-auto">
+                            {this.state.items.map(
+                                i => <NavBarItem
+                                    item={i}
+                                    onClick={this.onClickHandler}
+                                />)}
+                        </div>
+                    </div>
+                </nav>
             </div>
         );
     }
