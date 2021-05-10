66c733c06c95bcf228941c9d4dd3d8abf88441f2
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-10 15:37:42 -0300

    Criando o projeto Spring para o backend

---
+++ bluetasks-backend/src/test/java/augustobellinaso/bluetasksbackend/BluetasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package augustobellinaso.bluetasksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
