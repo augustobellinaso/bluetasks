f0735aaf398ff41de86deaa68e8c4e0b0c52a5e8
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-10 09:22:55 -0300

    Excluindo as tarefas

--- bluetasks-frontend/src/components/TaskListTable.js
+++ bluetasks-frontend/src/components/TaskListTable.js
@@ -8,13 +8,20 @@ class TaskListTable extends Component {
     this.state = {
       tasks: [],
     };
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+
+  componentDidMount() {
+    this.listTasks();
   }
 
   listTasks() {
     this.setState({ tasks: TaskService.list() });
   }
 
-  componentDidMount() {
+  onDeleteHandler(id) {
+    TaskService.delete(id);
     this.listTasks();
   }
 
@@ -22,7 +29,7 @@ class TaskListTable extends Component {
     return (
       <table className="table table-striped">
         <TableHeader />
-        <TableBody tasks={this.state.tasks} />
+        <TableBody tasks={this.state.tasks} onDelete={this.onDeleteHandler} />
       </table>
     );
   }
@@ -54,7 +61,12 @@ const TableBody = (props) => {
           <td>
             <input type="button" className="btn btn-primary" value="Editar" />
             &nbsp;
-            <input type="button" className="btn btn-danger" value="Excluir" />
+            <input
+              type="button"
+              className="btn btn-danger"
+              onClick={() => props.onDelete(task.id)}
+              value="Excluir"
+            />
           </td>
         </tr>
       ))}
