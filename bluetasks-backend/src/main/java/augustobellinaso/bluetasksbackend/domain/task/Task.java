7dc6016c638e57f9d98a45428001787fee929a95
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-11 09:51:44 -0300

    Adicionando validações nas entidades

--- bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/domain/task/Task.java
+++ bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/domain/task/Task.java
@@ -3,8 +3,12 @@ package augustobellinaso.bluetasksbackend.domain.task;
 import augustobellinaso.bluetasksbackend.domain.user.AppUser;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -16,14 +20,19 @@ public class Task {
     @GeneratedValue
     private Integer id;
 
+    @NotEmpty(message = "A descrição da tarefa é obrigatória")
+    @Length(min = 3, max = 40, message = "O tamanho da tarefa é inválido")
     private String description;
 
+    @NotNull(message = "A data da tarefa é obrigatória")
+    @FutureOrPresent(message = "A data da tarefa não pode estar no passado")
     private LocalDate whenToDo;
 
     private Boolean done = false;
 
     @ManyToOne
     @JoinColumn(name = "app_user_id")
+    @NotNull(message = "O usuário da tarefa é obrigatório")
     private AppUser appUser;
 
     public Task() {
--- bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/domain/user/AppUser.java
+++ bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/domain/user/AppUser.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "app_user")
@@ -18,10 +19,13 @@ public class AppUser {
     @GeneratedValue
     private Integer id;
 
+    @NotEmpty(message = "O nome de usuário é obrigatório")
     private String username;
 
+    @NotEmpty(message = "A senha é obrigatória")
     private String password;
 
+    @NotEmpty(message = "O nome de exibição é obrigatório")
     private String displayName;
 
     public AppUser() {
