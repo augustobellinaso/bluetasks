f6b6c6d5d744bda93588a675a892b7e030940ae5
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-11 09:41:58 -0300

    Expondo o ID das tarefas via web services

--- bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/BluetasksBackendApplication.java
+++ bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/BluetasksBackendApplication.java
@@ -1,13 +1,21 @@
 package augustobellinaso.bluetasksbackend;
 
+import augustobellinaso.bluetasksbackend.domain.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-public class BluetasksBackendApplication {
+public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluetasksBackendApplication.class, args);
 	}
 
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Task.class);
+	}
 }
