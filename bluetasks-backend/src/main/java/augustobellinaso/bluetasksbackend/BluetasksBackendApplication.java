cfcd6e9b2d00250e841d4fa4dab1a617ab3fa66b
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-11 10:00:48 -0300

    Configurando o processo de validação para retornar os erros corretamente

--- bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/BluetasksBackendApplication.java
+++ bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/BluetasksBackendApplication.java
@@ -3,8 +3,12 @@ package augustobellinaso.bluetasksbackend;
 import augustobellinaso.bluetasksbackend.domain.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
@@ -17,5 +21,17 @@ public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Task.class);
+	}
+
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+		Validator validator = validator();
+		validatingListener.addValidator("beforeCreate", validator);
+		validatingListener.addValidator("beforeSave", validator);
 	}
 }
