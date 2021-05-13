ad0ae30c4fc35f798d1b9f3e2a61195130a49976
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-13 11:08:53 -0300

    Formatando a data das tarefas

--- bluetasks-frontend/src/components/TaskListTable.js
+++ bluetasks-frontend/src/components/TaskListTable.js
@@ -6,6 +6,7 @@ import { Redirect } from "react-router-dom";
 import AuthService from "../api/AuthService";
 import Spinner from "./Spinner";
 import Alert from "./Alert";
+import Moment from "react-moment";
 
 class TaskListTable extends Component {
   constructor(props) {
@@ -129,7 +130,15 @@ const TableBody = (props) => {
             />
           </td>
           <td>{task.done ? <s>{task.description}</s> : task.description}</td>
-          <td>{task.done ? <s>{task.whenToDo}</s> : task.whenToDo}</td>
+          <td>
+            {task.done ? (
+              <s>
+                <Moment format="DD/MM/YYYY">{task.whenToDo}</Moment>
+              </s>
+            ) : (
+              <Moment format="DD/MM/YYYY">{task.whenToDo}</Moment>
+            )}
+          </td>
           <td>
             <input
               type="button"
