5f281f39d76b3dd89b6e990462102921ee1e89e6
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-17 13:48:56 -0300

    Resolvendo problema do CORS

--- bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/infrastructure/web/security/WebSecurityConfig.java
+++ bluetasks-backend/src/main/java/augustobellinaso/bluetasksbackend/infrastructure/web/security/WebSecurityConfig.java
@@ -2,17 +2,21 @@ package augustobellinaso.bluetasksbackend.infrastructure.web.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 
 @Configuration
-@EnableWebMvc
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private static Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
@@ -24,16 +28,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .disable()
                 .cors()
                 .and()
-                .headers().frameOptions().disable()
+                    .headers().frameOptions().disable()
                 .and()
-                .httpBasic()
+                    .httpBasic()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .anyRequest().authenticated()
+                    .authorizeRequests()
+                    .antMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                    .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                    .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
@@ -41,14 +45,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
         logger.info("Security setup... OK!");
     }
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("POST")
                 .allowedHeaders(SecurityConstants.AUTHORIZATION_HEADER);
 
         logger.info("CORS setup... OK!");
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList(SecurityConstants.AUTHORIZATION_HEADER));
+        configuration.addExposedHeader(SecurityConstants.AUTHORIZATION_HEADER);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
