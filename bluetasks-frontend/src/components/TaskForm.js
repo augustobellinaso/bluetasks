5bb7fe1bc1b719b85344aff42a70e8f4c7a134d6
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-21 15:10:15 -0300

    Cadastrando uma nova tarefa usando hooks

--- bluetasks-frontend/src/components/TaskForm.js
+++ bluetasks-frontend/src/components/TaskForm.js
@@ -1,155 +1,88 @@
-import React, { Component } from "react";
-import TaskService from "../api/TaskService";
+import React, { useContext, useState } from "react";
 import { Redirect } from "react-router-dom";
-import AuthService from "../api/AuthService";
-import Spinner from "./Spinner";
 import Alert from "./Alert";
+import { AuthContext } from "../hooks/useAuth";
+import { useTasks } from "../hooks/useTasks";
 
-class TaskForm extends Component {
-  constructor(props) {
-    super(props);
+const TaskForm = () => {
+  const auth = useContext(AuthContext);
+  const tasks = useTasks();
+  const [task, setTask] = useState({ id: 0, description: "", whenToDo: "" });
+  const [redirect, setRedirect] = useState(false);
 
-    this.state = {
-      task: {
-        id: 0,
-        description: "",
-        whenToDo: "",
-      },
-      redirect: false,
-      buttonName: "Cadastrar",
-      alert: null,
-      loading: false,
-      saving: false,
-    };
-
-    this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    this.onInputChangeHandler = this.onInputChangeHandler.bind(this);
-  }
-
-  componentDidMount() {
-    const editId = this.props.match.params.id;
-    if (editId) {
-      this.setState({ loading: true });
-      TaskService.load(
-        ~~editId,
-        (task) =>
-          this.setState({ task: task, loading: false, buttonName: "Alterar" }),
-        (error) => {
-          if (error.response) {
-            if (error.response.status === 404) {
-              this.setErrorState("Tarefa não encontrada");
-            } else {
-              this.setErrorState(`Erro ao carregar dados: ${error.response}`);
-            }
-          } else {
-            this.setErrorState(`Erro na requisição: ${error.message}`);
-          }
-        }
-      );
-    }
-  }
-
-  setErrorState(error) {
-    this.setState({
-      alert: error,
-      loading: false,
-      saving: false,
-    });
-  }
-
-  onSubmitHandler(event) {
+  const onSubmitHandler = (event) => {
     event.preventDefault();
-    this.setState({ saving: true, alert: null });
+    tasks.save(task);
+  };
 
-    TaskService.save(
-      this.state.task,
-      () => this.setState({ redirect: true, saving: false }),
-      (error) => {
-        if (error.response) {
-          this.setErrorState(`Erro: ${error.response.data.error}`);
-        } else {
-          this.setErrorState(`Erro na requisição: ${error.message}`);
-        }
-      }
-    );
-  }
-
-  onInputChangeHandler(event) {
+  const onInputChangeHandler = (event) => {
     const field = event.target.name;
     const value = event.target.value;
+    setTask({ ...task, [field]: value });
+  };
 
-    this.setState((prevState) => ({
-      task: { ...prevState.task, [field]: value },
-    }));
-    console.log(this.state.task);
+  if (!auth.isAuthenticated()) {
+    return <Redirect to="/login" />;
   }
 
-  render() {
-    if (!AuthService.isAuthenticated()) {
-      return <Redirect to="/login" />;
-    }
-
-    if (this.state.redirect) {
-      return <Redirect to="/" />;
-    }
-
-    if (this.state.loading) {
-      return <Spinner />;
-    }
-
-    return (
-      <div>
-        <h1>Cadastro de tarefa</h1>
-        {this.state.alert != null ? <Alert message={this.state.alert} /> : ""}
-        <form onSubmit={this.onSubmitHandler}>
-          <div className="form-group">
-            <label htmlFor="description">Descrição</label>
-            <input
-              type="text"
-              className="form-control"
-              name="description"
-              value={this.state.task.description}
-              placeholder="Digite a descrição"
-              onChange={this.onInputChangeHandler}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="whenToDo">Data</label>
-            <input
-              type="date"
-              className="form-control"
-              name="whenToDo"
-              value={this.state.task.whenToDo}
-              placeholder="Informe a data"
-              onChange={this.onInputChangeHandler}
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-success mr-2"
-            disabled={this.state.saving}
-          >
-            {this.state.saving ? (
-              <span
-                className="spinner-border spinner-border-sm"
-                role="status"
-                aria-hidden="true"
-              ></span>
-            ) : (
-              this.state.buttonName
-            )}
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={() => this.setState({ redirect: true })}
-          >
-            Cancelar
-          </button>
-        </form>
-      </div>
-    );
+  if (redirect || tasks.taskUpdated) {
+    return <Redirect to="/" />;
   }
-}
+
+  return (
+    <div>
+      <h1>Cadastro de tarefa</h1>
+      {tasks.error && <Alert message={tasks.error} />}
+      <form onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <label htmlFor="description">Descrição</label>
+          <input
+            type="text"
+            className="form-control"
+            name="description"
+            value={task.description}
+            placeholder="Digite a descrição"
+            onChange={onInputChangeHandler}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="whenToDo">Data</label>
+          <input
+            type="date"
+            className="form-control"
+            name="whenToDo"
+            value={task.whenToDo}
+            placeholder="Informe a data"
+            onChange={onInputChangeHandler}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-success mr-2"
+          disabled={tasks.processing}
+        >
+          {tasks.processing ? (
+            <span
+              className="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+          ) : task.id === 0 ? (
+            "Gravar"
+          ) : (
+            "Alterar"
+          )}
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={() => setRedirect(true)}
+        >
+          Cancelar
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default TaskForm;
